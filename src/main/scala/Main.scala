f4b99bc9e3a7b1fe882461c31f5cc62fedf5ba22
oluwaseyi <seyijava@gmail.com>
2022-04-15 10:35:32 -0300

    remove file

--- src/main/scala/Main.scala
+++
@@ -1,7 +0,0 @@
-import zio._
-import zio.console.putStrLn
-
-object Main extends App {
-  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
-    putStrLn("Welcome to your first ZIO app!").exitCode
-}
